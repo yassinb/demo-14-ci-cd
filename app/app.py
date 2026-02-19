3f6bf104dc90ae1be77968d59f81966dc6b3a8ad
Yassin Bellar <yassin.bellar@gmail.com>
2026-02-19 13:33:14 +0100

    update version to v4

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     version = os.getenv("APP_VERSION", "dev")
-    return f"Hello from CI/CD demo 🚀 V3 Version: {version}\n"
+    return f"Hello from CI/CD demo 🚀 V4 Version: {version}\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
